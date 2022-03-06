1ebe08e8e4bd469cd08440a4a46c51a4486f8b28
Varda Kari <varda.kari99@gmail.com>
2022-03-07 02:15:44 +0530

    update

--- client/typing-ui/src/App.js
+++ client/typing-ui/src/App.js
@@ -4,20 +4,20 @@ import Tester from './Components/Tester';
 import Validate from './Components/Validate';
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
 import Home from './Components/Home';
+import Context from './Context/Context';
 
 function App() {
   return (
     <BrowserRouter>
+    <Context.Provider value={{isLoggedIn: false}}>
       <Routes>
         <Route exact element={<Home/>} path="/"/>
         <Route element={<Tester/>} path="/typing-test"/>
         <Route element={<Validate/>} path="/validate"/>
         <Route element={<Home/>} path="*"/>
       </Routes>
+      </Context.Provider>
     </BrowserRouter>
-      // {/* <div className='bg-theme-secondary text-white py-2 px-5'>
-      //   <h2>Typing Test</h2>
-      // </div>*/}
   );
 }
 
---
+++ client/typing-ui/src/Context/Context.js
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+const Context = createContext({
+    isLoggedIn: false
+});
+
+export default Context 
